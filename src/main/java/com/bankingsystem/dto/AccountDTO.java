c1091941dd0cf55ea631274570147695b5e5783d
Mario Burgos <mario.burpi@gmail.com>
2023-02-15 13:32:41 +0100

    DTO creation

---
+++ src/main/java/com/bankingsystem/dto/AccountDTO.java
@@ -0,0 +1,132 @@
+package com.bankingsystem.dto;
+
+import com.bankingsystem.classes.Money;
+import com.bankingsystem.enums.Status;
+import com.bankingsystem.model.AccountHolder;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class AccountDTO {
+    //private String secretKey;
+    private String accountType;
+    private Long id;
+    private Money balance;
+    private AccountHolder primaryOwner;
+    private AccountHolder secondaryOwner;
+    private BigDecimal penaltyFee;
+    private BigDecimal minimumBalance;
+    private BigDecimal interestRate;
+    private BigDecimal monthlyMaintenanceFee;
+    private BigDecimal creditLimit;
+    private Date creationDate;
+    private Date lastInterestDate;
+    private Status status;
+
+    public AccountDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Money getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Money balance) {
+        this.balance = balance;
+    }
+
+    public AccountHolder getPrimaryOwner() {
+        return primaryOwner;
+    }
+
+    public void setPrimaryOwner(AccountHolder primaryOwner) {
+        this.primaryOwner = primaryOwner;
+    }
+
+    public AccountHolder getSecondaryOwner() {
+        return secondaryOwner;
+    }
+
+    public void setSecondaryOwner(AccountHolder secondaryOwner) {
+        this.secondaryOwner = secondaryOwner;
+    }
+
+    public BigDecimal getPenaltyFee() {
+        return penaltyFee;
+    }
+
+    public void setPenaltyFee(BigDecimal penaltyFee) {
+        this.penaltyFee = penaltyFee;
+    }
+
+    public BigDecimal getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    public void setMinimumBalance(BigDecimal minimumBalance) {
+        this.minimumBalance = minimumBalance;
+    }
+
+    public BigDecimal getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(BigDecimal interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public BigDecimal getMonthlyMaintenanceFee() {
+        return monthlyMaintenanceFee;
+    }
+
+    public void setMonthlyMaintenanceFee(BigDecimal monthlyMaintenanceFee) {
+        this.monthlyMaintenanceFee = monthlyMaintenanceFee;
+    }
+
+    public BigDecimal getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(BigDecimal creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastInterestDate() {
+        return lastInterestDate;
+    }
+
+    public void setLastInterestDate(Date lastInterestDate) {
+        this.lastInterestDate = lastInterestDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
