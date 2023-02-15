5fddd636ecf78f8ae23e72bcdb386a4b58b74a75
Mario Burgos <mario.burpi@gmail.com>
2023-02-15 13:34:57 +0100

    delete unused import

--- src/main/java/com/bankingsystem/repository/AdministratorRepository.java
+++ src/main/java/com/bankingsystem/repository/AdministratorRepository.java
@@ -4,8 +4,7 @@ import com.bankingsystem.model.Administrator;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface AdministratorRepository extends JpaRepository<Administrator, Long> {
+
 }
