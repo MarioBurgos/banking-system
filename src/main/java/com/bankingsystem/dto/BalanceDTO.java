71431c2bad80d7b215cf0e1daa565a2266e954cd
Mario Burgos <mario.burpi@gmail.com>
2023-02-16 11:31:42 +0100

    Added id and accountType properties

--- src/main/java/com/bankingsystem/dto/BalanceDTO.java
+++ src/main/java/com/bankingsystem/dto/BalanceDTO.java
@@ -5,7 +5,25 @@ import com.bankingsystem.classes.Money;
 import java.math.BigDecimal;
 
 public class BalanceDTO {
+    private Long accountId;
+    private String accountType;
     private BigDecimal amount;
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
 
     public BigDecimal getAmount() {
         return amount;
